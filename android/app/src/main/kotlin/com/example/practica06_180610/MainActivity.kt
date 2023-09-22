091912eecdf0688f33ed01294c55a9e1e972d8ed
FabricioFGV <62354571+FabricioFGV@users.noreply.github.com>
2023-09-22 14:39:31 -0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/practica06_180610/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.practica06_180610
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+//Creado por: Fabricio Guzman Vite
+//Asignatura: Desarrollo Movil Integral
+//Grado: 10 Grupo:A
+//Docente: MTI. Marco Antonio Ramirez Hernandez
+import 'package:flutter/material.dart';
+import 'package:practica06_180610/src/button.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: MyButton(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
+
